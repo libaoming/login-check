9fabe5ceb2018523c8a521e22c315924bc02a35c
master <libaoming2@gmail.com>
2016-08-25 19:09:02 +0800

    complete login check use core data

--- LogIn Check/ViewController.swift
+++ LogIn Check/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBOutlet weak var userTip: UILabel!
 
+
+    @IBAction func onLoginBtnTapped(_ sender: AnyObject) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+
+       
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        
+        newUser.setValue(textFeild.text, forKey: "username")
+        
+        do {
+            
+            try context.save()
+            
+            textFeild.alpha = 0
+            logInButton.alpha = 0
+            
+            userTip.text = "hi, \(textFeild.text!)"
+            
+            
+        }catch {
+            
+        }
+        
+    }
+    @IBOutlet weak var textFeild: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+           let results = try  context.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if  let username = result.value(forKey: "username") as? String {
+                        
+                        textFeild.text = username
+                        
+                        logInButton.alpha = 0
+                        textFeild.alpha = 0
+                        userTip.text = "hi , \(username)"
+                    }
+                    
+                    
+                    
+                    
+                    
+                }
+            }
+            
+            
+        } catch {
+            
+            
+        }
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
